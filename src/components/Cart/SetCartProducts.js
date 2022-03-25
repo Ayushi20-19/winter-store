2080b29a21ee1a187fc8eb9c72c50f6ec6f2baf0
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2022-03-26 02:27:11 +0530

    Remove comment

--- src/components/Cart/SetCartProducts.js
+++ src/components/Cart/SetCartProducts.js
@@ -1,19 +1,3 @@
-// import axios from "axios";
-
-// export const setCartProducts = async (product, localToken) => {
-//   const response = await axios.post(
-//     "/api/user/cart",
-// {
-//   product: {
-//     product,
-//   },
-//     },
-//     { headers: { authorization: localToken } }
-//   );
-
-//   // return response;
-// };
-
 import axios from "axios";
 
 export const setCartProducts = async (product, localToken) => {
